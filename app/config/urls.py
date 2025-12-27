a65fe2156298a0082ac3ee37d69068e894ba6de7
Bauyrzhan <billfookingmurray@gmail.com>
2025-12-27 20:43:54 +0500

    API Urls Added

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -15,10 +17,15 @@ urlpatterns = [
 
     # API v1
     path('api/v1/auth/', include('apps.users.urls')),
+    path('api/v1/rooms/', include('apps.rooms.urls')),
+    path('api/v1/bookings/', include('apps.bookings.urls')),
 
     # API Schema и документация
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
